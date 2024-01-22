06f38da03db093716626cf2c7237ba648566baa4
Rasul <Admin@ADMIN-PC>
2024-01-22 19:13:41 +0600

    Add project files.

---
+++ QA/BigInteger.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA
+{
+    internal class BigInteger<T>
+    {
+        private T value;
+
+        public BigInteger(T initialValue)
+        {
+            this.value = initialValue;
+        }
+
+        public T Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+
+        public static BigInteger<T> operator +(BigInteger<T> num1, BigInteger<T> num2)
+        {
+            dynamic result = Convert.ToDouble(num1.Value) + Convert.ToDouble(num2.Value);
+            return new BigInteger<T>((T)Convert.ChangeType(result, typeof(T)));
+            // Реализация сложения
+        }
+
+        public static BigInteger<T> operator -(BigInteger<T> num1, BigInteger<T> num2)
+        {
+            dynamic result = Convert.ToDouble(num1.Value) - Convert.ToDouble(num2.Value);
+            return new BigInteger<T>((T)Convert.ChangeType(result, typeof(T)));
+            // Реализация вычитания
+        }
+
+        public static BigInteger<T> operator *(BigInteger<T> num1, BigInteger<T> num2)
+        {
+            if (EqualityComparer<T>.Default.Equals(num2.Value, default(T)))
+            {
+                throw new ArgumentException("Умножение на ноль недопустимо.");
+            }
+            dynamic result = Convert.ToDouble(num1.Value) * Convert.ToDouble(num2.Value);
+            return new BigInteger<T>((T)Convert.ChangeType(result, typeof(T)));
+            // Реализация умножения
+        }
+
+        public static BigInteger<T> operator /(BigInteger<T> num1, BigInteger<T> num2)
+        {
+            if (EqualityComparer<T>.Default.Equals(num2.Value, default(T)))
+            {
+                throw new ArgumentException("Деление на ноль недопустимо.");
+            }
+            dynamic result = Convert.ToDouble(num1.Value) / Convert.ToDouble(num2.Value);
+            return new BigInteger<T>((T)Convert.ChangeType(result, typeof(T)));
+            // Реализация деления
+        }
+
+    }
+}
---
+++ QA/Program.cs
@@ -0,0 +1,46 @@
+﻿using QA;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Введите первое число:");
+            decimal num1 = decimal.Parse(Console.ReadLine()); // Чтение строки с консоли и преобразование в целое число
+
+            Console.WriteLine("Введите второе число:");
+            decimal num2 = decimal.Parse(Console.ReadLine()); // Чтение строки с консоли и преобразование в целое число
+
+            // Пример использования
+            BigInteger<decimal> bigInt1 = new BigInteger<decimal>(num1); // Создание объекта BigInteger с первым числом
+            BigInteger<decimal> bigInt2 = new BigInteger<decimal>(num2); // Создание объекта BigInteger со вторым числом
+
+            // Добавим проверку на ноль перед умножением
+            if (!EqualityComparer<decimal>.Default.Equals(num2, 0)) // Проверка на ноль перед умножением
+            {
+                BigInteger<decimal> product = bigInt1 * bigInt2; // Выполнение операции умножения
+                Console.WriteLine("Product: " + product.Value); // Вывод результата умножения
+            }
+            else
+            {
+                Console.WriteLine("Умножение на ноль недопустимо."); // Вывод сообщения об ошибке при умножении на ноль
+            }
+
+            // Проверку на ноль перед делением
+            if (!EqualityComparer<decimal>.Default.Equals(num2, 0)) // Проверка на ноль перед делением
+            {
+                BigInteger<decimal> division = bigInt1 / bigInt2; // Выполнение операции деления
+                Console.WriteLine("Результат: " + division.Value); // Вывод результата деления
+            }
+            else
+            {
+                Console.WriteLine("Деление на ноль недопустимо."); // Вывод сообщения об ошибке при делении на ноль
+            }
+        }
+        catch (Exception ex) // Обработка исключений
+        {
+            Console.WriteLine("Ошибка: " + ex.Message); // Вывод сообщения об ошибке
+        }
+    }
+}
